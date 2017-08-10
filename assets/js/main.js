fc941f2669ee51d18637c6aa7e93425d0388cc49
linjianghe <303522560@qq.com>
2017-08-10 23:06:30 +0800

    增加js文件

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright lin 2017
+ */
+
+$(document).ready( function() {
+
+    //左菜单栏
+    $('.templatemo-sidebar-menu li.sub a').click(function(){
+        if($(this).parent().hasClass('open')) {
+            $(this).parent().removeClass('open');
+        } else {
+            $(this).parent().addClass('open');
+        }
+    });
+
+	//管理员
+	$(".admin_edit").click(function() {
+		adminid = $(this).data('id');
+		$.post("/admin/get_info", {id:adminid}, function(data){
+			$("#adminid").val(data.data.id);
+			$("#username").val(data.data.username);
+			$("#name").val(data.data.name);
+		},'json');
+		$("#myModal").modal();
+	});
+
+});
+
+function save(){
+    var adminid = $("#adminid").val();
+    var username = $("#username").val();
+    var name = $("#name").val();
+    $.post('/admin/update',{id:adminid,username:username,name:name}, function(data){
+        if(data.code==200){
+            $('#myModal').modal('hide');
+            window.location.reload();
+        } else {
+            alert('修改失败');
+        }
+    },'json');
+}
